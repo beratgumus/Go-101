bed4c1c5eb0b7272f70ae5eb3f3c408cbf9402b2
Berat Gumus <beratgumus9@gmail.com>
2018-03-04 17:44:53 +0300

    defer

---
+++ defer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Defer
+	fmt.Println("\n\nDefer:")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%d ", i)
+		// Deferred things are added to a stack and executed after func return
+		defer fmt.Printf("%d ", i)
+	}
+	fmt.Println()
+
+	defer first()
+	second()
+}
+
+func first(){
+	fmt.Println("this is func first")
+}
+
+func second(){
+	fmt.Println("this is func second")
+}
